353b6f3c3abb9816276556dfdf5d618c0e3eeb45
Ana Bebic <anabebic.x@gmail.com>
2018-05-25 16:43:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`;
 }
 
-function holidayCountdown(holiday,days){
-  return"It's ${days} until ${holiday}!"
-  
+
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`;
 }
-function holidayCountdown(20,"Mother's Day");
